1c4a583b87aba8986979d8cd259ab3711db14c08
Coder Sensui <137837037+CoderSensui@users.noreply.github.com>
2026-02-28 22:41:50 +0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,3 +1,11 @@
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(reg => console.log('Service Worker Registered!', reg))
+            .catch(err => console.error('Service Worker Registration Failed!', err));
+    });
+}
 document.addEventListener('DOMContentLoaded', () => {
 
     function renderIcons() { if(typeof lucide !== 'undefined') lucide.createIcons(); }
